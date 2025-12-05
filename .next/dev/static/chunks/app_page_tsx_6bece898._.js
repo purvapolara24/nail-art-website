905f8a0d8c54a7323ff8d18f98eea68fc3030a04
purvapolara24 <purva@Purva>
2025-12-05 20:43:17 +0530

    Update .gitignore to exclude Next.js build files

--- .next/dev/static/chunks/app_page_tsx_6bece898._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_f346b63b._.js",
-  "static/chunks/_72ac3303._.js"
-],
-    source: "dynamic"
-});
